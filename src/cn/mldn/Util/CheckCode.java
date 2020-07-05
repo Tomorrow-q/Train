ec1b93918c79c46743dc7211f0a0189f932d2732
Tomorrow-ting <2309094255@qq.com>
2020-07-05 16:44:00 +0800

    火车售票

--- src/cn/mldn/Util/CheckCode.java
+++ src/cn/mldn/Util/CheckCode.java
@@ -20,70 +20,70 @@ private int jam = 5;//干扰元素 建议使用 4~7 之间的数字
 private String code = "";//保存验证码
  
 public CheckCode(){//初始化窗体信息
-super("验证码");
-setVisible(true);
-setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-300)/2, (Toolkit.getDefaultToolkit().getScreenSize().height-300)/2, 300, 200);
-setDefaultCloseOperation(EXIT_ON_CLOSE);
-addMouseListener(new MouseAdapter() {
-public void mouseClicked(MouseEvent e) {
-repaint();
-}
-});
+	super("验证码");
+	setVisible(true);
+	setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-300)/2, (Toolkit.getDefaultToolkit().getScreenSize().height-300)/2, 300, 200);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	addMouseListener(new MouseAdapter() {
+		public void mouseClicked(MouseEvent e) {
+			repaint();
+		}
+	});
 }
  
 public Color getRandomColor(){//获得随机颜色
-int R=random.nextInt(255),G=random.nextInt(255),B=random.nextInt(255);
-return new Color(R,G,B);
+	int R=random.nextInt(255),G=random.nextInt(255),B=random.nextInt(255);
+	return new Color(R,G,B);
 }
  
 public String getRandomString(){//获得验证码
-int num = random.nextInt(9);
-code = num+"";
-return num+"";
+	int num = random.nextInt(9);
+	code = num+"";
+	return num+"";
 }
  
 public void checkCode(Graphics g){// 绘画验证码
-drawBorder(g); 
-drawCode(g);
-drawJam(g);
+	drawBorder(g); 
+	drawCode(g);
+	drawJam(g);
 }
  
 public void drawBorder(Graphics g){//绘画边框和背景
-Color gc = g.getColor();
-g.setColor(Color.WHITE);
-g.fillRect(x, y, width, height);
-g.setColor(Color.BLACK);
-g.drawRect(x, y, width, height);
-g.setColor(gc);
+	Color gc = g.getColor();
+	g.setColor(Color.WHITE);
+	g.fillRect(x, y, width, height);
+	g.setColor(Color.BLACK);
+	g.drawRect(x, y, width, height);
+	g.setColor(gc);
 }
  
 public void drawCode(Graphics g){//绘画验证码内容
-Color gc = g.getColor();
-for(int i=0;i<4;i++){
-g.setColor(getRandomColor());
-g.setFont(new Font("宋体",Font.BOLD,font_size));
-g.drawString(getRandomString(), x+5+(i*12), y+font_size);
+	Color gc = g.getColor();
+	for(int i=0;i<4;i++){
+		g.setColor(getRandomColor());
+		g.setFont(new Font("宋体",Font.BOLD,font_size));
+		g.drawString(getRandomString(), x+5+(i*12), y+font_size);
 }
-g.setColor(gc);
+	g.setColor(gc);
 }
  
 public void drawJam(Graphics g){//绘画干扰元素
-Color gc = g.getColor();
-for(int i=0;i<jam;i++){
-g.setColor(getRandomColor());
-g.drawLine(x+random.nextInt(width), y+random.nextInt(height), x+random.nextInt(width), y+random.nextInt(height));
+	Color gc = g.getColor();
+	for(int i=0;i<jam;i++){
+		g.setColor(getRandomColor());
+		g.drawLine(x+random.nextInt(width), y+random.nextInt(height), x+random.nextInt(width), y+random.nextInt(height));
 }
-g.setColor(gc);
+	g.setColor(gc);
 }
  
 public void paint(Graphics g) {
-Color c = g.getColor();
-g.drawString("单击可刷新验证码", 30, 50);
-checkCode(g);
-g.setColor(c);
+	Color c = g.getColor();
+	g.drawString("单击可刷新验证码", 30, 50);
+	checkCode(g);
+	g.setColor(c);
 }
  
 public static void main(String[] args) {
-new CheckCode();
+	new CheckCode();
 }
 }
